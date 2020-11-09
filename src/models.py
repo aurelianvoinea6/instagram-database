cb4cc0d2992bc6a5b4e54b936ce0467111b225f2
aurelianvoinea6 <aurash1991@hotmail.com>
2020-11-09 21:30:30 +0000

    databasemodel

--- src/models.py
+++ src/models.py
@@ -10,24 +10,41 @@ Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+  
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    posts = relationship('Post')
+
+class Image(Base):
+    __tablename__ = 'image'
+ 
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    url = Column(String(250), nullable=False)
+    size = Column(String(250), nullable=False)
+    person = relationship('Post', back_populates='image')
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    date = Column(String(250), nullable=False)
+    likes = Column(String(250), nullable=True)
+    image = relationship('Image', uselist=False, back_populates='post')
 
 class Address(Base):
     __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    person = relationship('Person')
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
 render_er(Base, 'diagram.png')
