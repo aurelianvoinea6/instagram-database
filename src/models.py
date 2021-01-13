112caf92a9d8ef04a6c9518bcb7b1c201e185ad1
aurelianvoinea6 <aurash1991@hotmail.com>
2021-01-13 14:28:49 +0000

    modified the database models

--- src/models.py
+++ src/models.py
@@ -8,43 +8,80 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
   
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
     username = Column(String(250), nullable=False)
-    posts = relationship('Post')
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Image(Base):
-    __tablename__ = 'image'
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.userName,
+            "email": self.email,
+        }
+
+class Follower(Base):
+    __tablename__ = 'follower'
  
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "user_from_id": self.user_from_id,
+        }
+    
+
+class Comments(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
-    url = Column(String(250), nullable=False)
-    size = Column(String(250), nullable=False)
-    person = relationship('Post', back_populates='image')
+    comment_text = Column(String(250), nullable=True)
+   
+    def serialize(self):
+       return {
+           "id": self.id,
+           "author_id": self.user_id,
+           "post_id": self.post_id,
+           "comment_text": self.text,
+       }
 
 class Post(Base):
     __tablename__ = 'post'
-    id = Column(Integer, primary_key=True)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    date = Column(String(250), nullable=False)
-    likes = Column(String(250), nullable=True)
-    image = relationship('Image', uselist=False, back_populates='post')
-
-class Address(Base):
-    __tablename__ = 'address'
 
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship('Person')
+    user_id = Column(Integer, ForeignKey('user.id'))
 
-    def to_dict(self):
-        return {}
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+        }
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    url = Column(String(250), nullable=False)
+    type_media = Column(Integer, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "post_id": self.post_id,
+            "url": self.url,
+            "type_media": self.type_media,
+        }
+
 
 
 render_er(Base, 'diagram.png')
